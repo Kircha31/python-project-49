eaf26a248ca077e8c5d28fa3e40895eeef81246b
KirillKolomoets <kolomoet.kirill@yandex.ru>
2023-03-28 20:18:53 +0400

    making a correction in the text which output user

--- brain_games/games/ans_que.py
+++ brain_games/games/ans_que.py
@@ -3,32 +3,39 @@ from typing import List
 
 import prompt
 
+
 # generate random number
 def number():
     question = randint(0, 100)
     return question
+
 
 # Question formation and receiving answer
 def que_ans(numb):
     answer = prompt.string(f"Question: {numb}\nYour answer: ")
     return answer
 
+
 # Random math sign
 def sign():
     signs = ['*', '+', '-']
     return choice(signs)
 
+
 # multiplication
 def multiplication(a, b):
     return a * b
+
 
 # addition
 def addition(a, b):
     return a + b
 
+
 # subtraction
 def subtraction(a, b):
     return a - b
+
 
 # Formation answer on the action user
 def post_response(status, player='', answer='', total=''):
@@ -40,6 +47,7 @@ Let's try again, {player}!"""
     else:
         return f'Congratulations, {player}!'
 
+
 # Greet users
 def welcome_user(game):
     name = prompt.string('May I have your name? ')
@@ -47,14 +55,15 @@ def welcome_user(game):
     if game == 'calc_game':
         print('What is the result of the expression?')
     elif game == 'even_game':
-        print('Answer "yes" if the number is even, otherwise answer "no"')
+        print('Answer "yes" if the number is even, otherwise answer "no".')
     elif game == 'prime_game':
-        print('Answer "yes" if given number is prime. Otherwise answer "no"')
+        print('Answer "yes" if given number is prime. Otherwise answer "no".')
     elif game == 'arfprog_game':
         print('What number is missing in the progression?')
     else:
         print("Find the greatest common divisor of given numbers.")
     return name
+
 
 # Check corretly answer
 def check(answer):
@@ -66,12 +75,14 @@ def check(answer):
     else:
         return False
 
+
 # Check is it number of zero
 def check_zero(numb1, numb2):
     if numb1 == 0:
         return numb2
     else:
         return numb1
+
 
 # Find max divisor number
 def gcd_number(numb1, numb2):
@@ -84,6 +95,7 @@ def gcd_number(numb1, numb2):
         if numb1 % i == 0 and numb2 % i == 0:
             total.append(i)
     return max(total)
+
 
 # create list for arifmetik progression
 def create_list():
@@ -104,6 +116,7 @@ def simple_number(numb):
         return 'yes'
     else:
         return 'no'
+
 
 # create list
 def create_full_list():
