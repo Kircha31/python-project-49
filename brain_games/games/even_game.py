354327bc3b7b66bcd4a8339bbcbc522c2f484bb9
KirillKolomoets <kolomoet.kirill@yandex.ru>
2023-03-29 17:53:00 +0400

    Debug - 4

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -1,30 +1,33 @@
 from brain_games.ans_que import *
-import sys, os
 
 
 def even():
     player = welcome_user('even_game')
     count = 3
+    ans_use = ''
     while count > 0:
         question = number()
         answer = que_ans(question)
         if answer not in ['yes', 'no']:
-            quit(post_response('lose', player, answer, 'yes/no'))
-            sys.exit(os.EX_OK)
+            ans_use = 'yes/no'
+            break
         elif question % 2 == 0:
             if check(answer):
                 print(post_response('right'))
                 count -= 1
             else:
-                sys.exit(post_response('lose', player, answer, 'no'))
+                ans_use = 'no'
+                break
         else:
             if not check(answer):
                 print(post_response('right'))
                 count -= 1
             else:
-                sys.exit(post_response('lose', player, answer, 'yes'))
+                ans_use = 'yes'
+                break
     
     if count == 0:
         print(post_response('win', player))
-        sys.exit(os.EX_OK)
+    else:
+        print(post_response('lose', player, answer, ans_use))
         
