355715470a066cf1abe8f648d9709f3319abfe98
KirillKolomoets <kolomoet.kirill@yandex.ru>
2023-03-29 16:53:08 +0400

    Debug exit metod

--- brain_games/games/even_game.py
+++ brain_games/games/even_game.py
@@ -9,22 +9,23 @@ def even():
         question = number()
         answer = que_ans(question)
         if answer not in ['yes', 'no']:
-            x = post_response('lose', player, answer, 'yes/no')
-            sys.exit(x)
+            print(post_response('lose', player, answer, 'yes/no'))
+            return False
         elif question % 2 == 0:
             if check(answer):
                 print(post_response('right'))
                 count -= 1
             else:
-                x = post_response('lose', player, answer, 'no')
-                sys.exit(x)
+                print(post_response('lose', player, answer, 'no'))
+                return False
         else:
             if check(answer):
                 print(post_response('right'))
                 count -= 1
             else:
-                x = post_response('lose', player, answer, 'yes')
-                sys.exit(x)
+                print(post_response('lose', player, answer, 'yes'))
+                return False
     
     if count == 0:
-        exit(post_response('win', player))
+        print(post_response('win', player))
+        return True
--- brain_games/scripts/brain_even.py
+++ brain_games/scripts/brain_even.py
@@ -1,9 +1,14 @@
 from brain_games.games import even_game as e
-
+import sys
 
 def main():
     print('Welcome to the Brain Games!')
-    e.even()
+    
+    output = e.even()
+    if output:
+        sys.exit(0)
+    else:
+        sys.exit(0)
 
 
 if __name__ == '__main__':
