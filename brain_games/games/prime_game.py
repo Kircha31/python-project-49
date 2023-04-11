9d009a93d1aa8249cdc8341664b60238d04dfa49
KirillKolomoets <kolomoet.kirill@yandex.ru>
2023-04-11 08:46:00 +0400

    Debug mistakes-6

--- brain_games/games/prime_game.py
+++ brain_games/games/prime_game.py
@@ -4,18 +4,15 @@ from random import choice
 RULES = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def get_check_number(numb):
+def is_prime(numb):
     k = 0
     for i in range(2, numb):
         if numb % i == 0:
             k += 1
-    if k == 0:
-        return True
-    else:
-        return False
+    return True if k == 0 else False
 
 
 def get_question_and_right_answer():
     ran_numb = choice([x for x in range(1, 100)])
-    correct = 'yes' if get_check_number(ran_numb) else 'no'
+    correct = 'yes' if is_prime(ran_numb) else 'no'
     return ran_numb, correct
